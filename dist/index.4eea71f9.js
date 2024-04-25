f4010e142ef1603d050fe4da1e0dad957a3c72bf
Yurii Shkrobut <georgebest2409@gmail.com>
2024-04-25 15:37:44 +0300

    make build

---
+++ dist/index.4eea71f9.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector("#form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.4eea71f9.js.map
